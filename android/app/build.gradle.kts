a2fda21c188bbc1f21b568e31a2f773e1a467a0e
andyb3rry <and.baraldi@gmail.com>
2026-07-11 00:12:58 +0200

    Updated appId

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.servercommander.server_commander"
+    namespace = "com.android.sshdashboard"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.servercommander.server_commander"
+        applicationId = "com.android.sshdashboard"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
